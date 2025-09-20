cc38707abbd74bf7882178eb60ded61caddc571b
Glauco Pater <glauco.pater@gmail.com>
2025-09-20 20:19:05 +0200

    Media Generator filtered by feature flag

--- src/main.ts
+++ src/main.ts
@@ -11,26 +11,39 @@ import { UploadFilesCard } from "./components/UploadImageCard.ts";
 import { UploadProgressModal } from "./components/UploadProgressModal.ts";
 import { GenerateMediaCard } from "./components/GenerateMediaCard.ts";
 
+
+const isMediaGeneratorEnabled = import.meta.env.VITE_ENABLE_MEDIA_GENERATOR === "true";
+
+
+const mediaGeneratorTab = isMediaGeneratorEnabled ? `
+ <li class="nav-item" role="presentation">
+    <button class="nav-link" id="generate-tab" data-bs-toggle="tab" data-bs-target="#generate-tab-pane" type="button" role="tab" aria-controls="generate-tab-pane" aria-selected="false">Generate Image</button>
+ </li>` : '';
+
+const mediaGeneratorContent = isMediaGeneratorEnabled ?
+  `<div class="tab-pane" id="generate-tab-pane" role="tabpanel" aria-labelledby="generate-tab" tabindex="0">'
+    ${GenerateMediaCard()}
+  </div>`
+  : '';
+
+
+
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="min-vh-100 d-flex flex-column">
     ${Header(logo)}
-
     <div class="container mt-4">
       <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
           <button class="nav-link active" id="analyze-tab" data-bs-toggle="tab" data-bs-target="#analyze-tab-pane" type="button" role="tab" aria-controls="analyze-tab-pane" aria-selected="true">Analyze Media</button>
         </li>
-        <li class="nav-item" role="presentation">
-          <button class="nav-link" id="generate-tab" data-bs-toggle="tab" data-bs-target="#generate-tab-pane" type="button" role="tab" aria-controls="generate-tab-pane" aria-selected="false">Generate Image</button>
-        </li>
+         ${mediaGeneratorTab}           
       </ul>
       <div class="tab-content" id="myTabContent">
         <div class="tab-pane show active" id="analyze-tab-pane" role="tabpanel" aria-labelledby="analyze-tab" tabindex="0">
           ${VisionExperiment(UploadFilesCard(UploadProgressModal()))}
         </div>
-        <div class="tab-pane" id="generate-tab-pane" role="tabpanel" aria-labelledby="generate-tab" tabindex="0">
-          ${GenerateMediaCard()}
-        </div>
+        ${mediaGeneratorContent}      
       </div>
     </div>
 
